9e7cb1f7c1e0c2855088b60a0dac6b68de1324e4
FarahJamal <farahsarese@gmail.com>
2021-09-28 17:24:09 +0300

    finished with stretch goal

--- basicLibrary/lib/src/test/java/basicLibrary/LibraryTest.java
+++ basicLibrary/lib/src/test/java/basicLibrary/LibraryTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LibraryTest {
@@ -17,10 +20,27 @@ Library test;
     @Test
     @DisplayName("Simple average should work")
     void testAverage(){
-        //[2,5,5,8,10]
         int [] arr=new int []{2,5,5,8,10};
         assertEquals(6, test.average(arr),"Regular average should work");
 
+
+    }
+
+    @Test
+    @DisplayName("tally function should out the most votes")
+    void testTally(){
+        List<String> votes = new ArrayList<>();
+        votes.add("snape");
+        votes.add("snape");
+        votes.add("voldemort");
+        votes.add("snape");
+        votes.add("voldemort");
+        votes.add("snape");
+        votes.add("harry_potter");
+        votes.add("snape");
+        votes.add("harry_potter");
+
+        assertEquals("snape", test.tally(votes),"tally votes should work");
 
     }
 }
